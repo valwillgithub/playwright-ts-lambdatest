96972d3a757c1636987a6e6a693c69031790158f
valwillgithub <vastyjay@yahoo.com>
2025-08-15 00:04:31 +0100

    vscode initial commit

---
+++ tests/returnMethodAttribute.test.ts
@@ -0,0 +1,15 @@
+import { Page } from '@playwright/test';
+/** 
+<form action="/submit" method="post" class="loginForm">
+  <input type="text" name="name" placeholder="username">
+  <input type="password" name="password" placeholder="password">
+  <input type="submit" name="submit" value="submit">
+</form>
+*/
+async function grtFormMethodAttribute(page: Page): Promise<string | null> {
+  const loginForm = page.locator('form.loginForm');
+  if (!loginForm) return null;
+  let postText = await loginForm.getAttribute('post');
+  console.log('Post text: => ' + postText);
+  return postText;
+}
