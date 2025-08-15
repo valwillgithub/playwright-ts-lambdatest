503ba6e4568e4f167240be5649a64e8e8471a868
valwillgithub <vastyjay@yahoo.com>
2025-08-15 01:35:14 +0100

    added dev-branch text

--- tests/returnMethodAttribute.test.ts
+++ tests/returnMethodAttribute.test.ts
@@ -10,6 +10,7 @@ async function grtFormMethodAttribute(page: Page): Promise<string | null> {
   const loginForm = page.locator('form.loginForm');
   if (!loginForm) return null;
   let postText = await loginForm.getAttribute('post');
-  console.log('Post text: => ' + postText);
+  console.log('Post text: Modified ' + postText);
+  console.log('Post text in dev-branch: ' + postText);
   return postText;
 }
